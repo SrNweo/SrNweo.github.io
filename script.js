4b0bb95ec2305267e9adb0e080c17cbefe59b05e
imHyro <srnweo@gmail.com>
2022-02-08 20:27:54 -0500

    small design update
    - code optimization
    - unnecessary code removed
    - social network buttons added (using svg icons)
    - ko fi button

--- script.js
+++ script.js
@@ -1,86 +1,12 @@
-btn = document.getElementById("chb");
+dsc = document.getElementById("discord");
 
-btn.addEventListener("click", sDM);
+dsc.addEventListener("click", getDscTag)
 
-dme = 0;
-
-dmc = window.localStorage.getItem("dm");
-
-if (dmc == 1) {
-    document.getElementById("chb").checked = true;
-
-    dme = 0;
-
-    htmlvar = document.getElementById("header");
-
-    htmlvar.style.backgroundColor = "#000";
-
-    htmlvar = document.getElementById("nav");
-
-    htmlvar.style.backgroundColor = "#000";
-
-    htmlvar.style.color = "#fff";
-
-    htmlvar = document.getElementById("body");
-
-    htmlvar.style.backgroundColor = "#000";
+function getDscTag() {
+    navigator.clipboard.writeText("SrNweo#3982");
+    alert("My tag has been copied to your clipboard!");
 }
 
-function sDM() {
-    if (dme == 0) {
-        htmlvar = document.getElementById("header");
-
-        htmlvar.style.backgroundColor = "#000";
-
-        htmlvar.style.transition = "1s";
-
-        htmlvar = document.getElementById("nav");
-
-        htmlvar.style.backgroundColor = "#000";
-
-        htmlvar.style.color = "#fff";
-
-        htmlvar.style.transition = "1s";
-
-        htmlvar = document.getElementById("body");
-
-        htmlvar.style.backgroundColor = "#000";
-
-        htmlvar.style.transition = "1s";
-
-        dme = 1;
-        if (dmc == null) {
-            if (
-                confirm(
-                    "Would you like to save your dark mode preferences from now on?"
-                )
-            ) {
-                window.localStorage.setItem("dm", "1");
-            } else {
-                window.localStorage.setItem("dm", "0");
-            }
-        }
-    } else {
-        htmlvar = document.getElementById("header");
-
-        htmlvar.style.backgroundColor = "#222";
-
-        htmlvar.style.transition = "1s";
-
-        htmlvar = document.getElementById("nav");
-
-        htmlvar.style.backgroundColor = "#aaa";
-
-        htmlvar.style.color = "#000";
-
-        htmlvar.style.transition = "1s";
-
-        htmlvar = document.getElementById("body");
-
-        htmlvar.style.backgroundColor = "#fff";
-
-        htmlvar.style.transition = "1s";
-
-        dme = 0;
-    }
+function openInNewTab(url) {
+    window.open(url, '_blank').focus();
 }
