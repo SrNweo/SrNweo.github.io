10a2ed34a79f4d57f7233e8f437d9ba080cb1448
imHyro <srnweo@gmail.com>
2022-02-13 17:12:23 -0500

    1.3 | update
    - fixed minimum typing error in title
    - added a mobile browser checker and a layout incompatibility notice
    - made minor changes to some texts
    - descriptions for social network icons
    - new discord logo design
    - instagram link added
    - the tab title now says the "version" of the page
    - removed an unused function
    - extra details to each icon

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+version = "1.3";
+
+document.title = "SrNweo | " + version;
+
 dsc = document.getElementById("discord");
 
 dsc.addEventListener("click", getDscTag)
@@ -5,8 +9,4 @@ dsc.addEventListener("click", getDscTag)
 function getDscTag() {
     navigator.clipboard.writeText("SrNweo#3982");
     alert("My tag has been copied to your clipboard!");
-}
-
-function openInNewTab(url) {
-    window.open(url, '_blank').focus();
 }
